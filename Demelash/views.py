dd3f425d47a0c83af50fd25675f4a5e845fd2749
demelash1452@gmail.com <demelash1452@gmail.com>
2022-04-23 14:00:09 +0300

    Update index file

--- Demelash/views.py
+++ Demelash/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portfolio
 
+# Home
+home = Home.objects.latest('updated')
+
+context = {
+        'home': home,
+
+    }
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
