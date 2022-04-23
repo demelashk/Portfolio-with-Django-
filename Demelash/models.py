801d49428e6a81f5067d73ae335859923b98854e
demelash1452@gmail.com <demelash1452@gmail.com>
2022-04-23 18:11:04 +0300

    Update index file

--- Demelash/models.py
+++ Demelash/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Home(models.Model):
     name = models.CharField(max_length=20)
-    greetings_1 = models.CharField(max_length=50)
-    greetings_2 = models.CharField(max_length=50)
+    greetings_1 = models.CharField(max_length=5)
+    greetings_2 = models.CharField(max_length=5)
     picture = models.ImageField(upload_to='picture/')
     # save time when modified
     updated = models.DateTimeField(auto_now=True)
